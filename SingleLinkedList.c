9a5ea3d44629d7218c62debc9ee3163f9a3473fd
Jayasurya R <62130969+Jayasurya-Ramalingam@users.noreply.github.com>
2026-05-20 09:33:42 +0530

    Add insertAtPos and deletePos functions

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -63,6 +63,44 @@ void insertAtBegin(struct Node **temp, int dat)
     *temp = newNode;
 }
 
+void insertAtPos(struct Node **temp,int data, int Pos)
+{
+    struct Node *head = createNode(data);
+    struct Node *trace = *temp;
+    int Counter=0;
+    
+    while(!(Counter!=Pos))
+    {
+        trace = trace->next;
+        Counter++;
+    }
+    
+    struct Node *newNode = trace->next;
+    head->next = newNode;
+    trace->next = head;
+    
+    *temp = trace;
+}
+
+void deletePos(struct Node **temp, int Pos)
+{
+    struct Node *trace = *temp;
+    int count=0;
+    while(!(count==Pos-1))
+    {
+        trace = trace->next;
+        count++;
+    }
+    
+    struct Node *freeNode = trace->next;
+    
+    trace->next = trace->next->next;
+    
+    free(freeNode);
+    
+    //*temp = trace;
+}
+
 int main() {
 	// your code goes here
     struct Node* obj = createNode(10);
@@ -79,5 +117,14 @@ int main() {
     
     insertAtBegin(&obj,22);
     display(obj);
+    
+    printf("After Deleting the node\n");
+    deletePos(&obj,3);
+    display(obj);
+
+    printf("After Inserting the node\n");    
+    insertAtPos(&obj,44,2);
+    display(obj);
+    
 }
 
